2910c31977a0c88d8c25f72f3d32978bcf2b58a5
Dennis <den.peld@gmail.com>
2020-04-16 11:09:56 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOLID.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
